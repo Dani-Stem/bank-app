56e2e0e561490f73a3954e30074376c693421f47
dd <dannniellyyy@gmail.com>
2025-10-17 20:36:04 -0700

    join statement instead of select where where

--- accounts_db.py
+++ accounts_db.py
@@ -17,20 +17,15 @@ CREATE TABLE IF NOT EXISTS users (
 # cursor.execute(create_table_query)
 # cursor.execute("INSERT INTO users (name, login, password, account_num) VALUES (?, ?, ?, ?)", ("Dani stem", "dd1234", "angel", 1))
 
-cursor.execute("SELECT number FROM accounts WHERE number = (select account_num from users where login = 'dd1234')")
+cursor.execute("select number from accounts JOIN users on accounts.Number = users.account_num where users.login = 'dd1234'")
 all_users = cursor.fetchone()
-
 
 print("All users in the database:")
 # for user in all_users:
-clean_data = ''.join(str(all_users))
-cleaner_data = clean_data.replace(",","").replace("(","").replace(")","")
-print(cleaner_data)
-
-
+clean_data = int(all_users[0])
+print(clean_data)
 
 # cursor.execute("INSERT INTO accounts (Number, Owner, Balance) VALUES (?, ?, ?)", (f"{number}", f"{owner}", "0"))
-
 
 # cursor.execute("SELECT Owner FROM accounts WHERE Number = 1")
 # all_users = cursor.fetchall()
