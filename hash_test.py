bc85eab36b7b5c1510ad440ae12e7ca3d62bd4bd
dd <dannniellyyy@gmail.com>
2025-10-29 22:46:47 -0700

    sync

--- hash_test.py
+++ hash_test.py
@@ -1,10 +1,12 @@
 import bcrypt
+test = "wrongpassword"
+user_input = "ab"
 
-user_input = input("Enter Password: ")
 password = user_input.encode('utf-8')
 salt = bcrypt.gensalt()
+print(salt)
 hashed_password = bcrypt.hashpw(password, salt)
 print(f"Hashed password: {hashed_password}")
 
-is_correct = bcrypt.checkpw(test, hashed_password)
+is_correct = bcrypt.checkpw(test.encode('utf-8'), hashed_password)
 print(f"Verification (correct password): {is_correct}")
